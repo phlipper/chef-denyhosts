251c66535450c254518d7584750e9da6ff11b7de
Phil Cohen <github@phlippers.net>
2013-11-06 10:39:50 -0800

    bump version to 1.4.0

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       "Phil Cohen"
 maintainer_email "github@phlippers.net"
 license          "MIT"
 description      "Installs denyhosts"
-version          "1.3.1"
+version          "1.4.0"
 
 %w{ubuntu debian redhat centos}.each do |os|
   supports os
